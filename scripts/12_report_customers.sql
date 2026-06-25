27dc32c33b946ac01998d0b23a49cd73e63b972c
Adam Mercadante <ajmerc11@gmail.com>
2026-06-25 14:49:13 -0600

    Update 12_report_customers.sql

--- scripts/12_report_customers.sql
+++ scripts/12_report_customers.sql
@@ -34,27 +34,27 @@ WITH base_query AS (
 /*---------------------------------------------------------------------------
 1) Base Query: Retrieves core columns from tables
 ---------------------------------------------------------------------------*/
-SELECT
-	s.order_number,
-	s.product_key,
-	s.order_date,
-	s.sales_amount,
-	s.quantity,
-	c.customer_key,
-	c.customer_number,
-	CONCAT(c.first_name, ' ', c.last_name) AS customer_name, 
-	DATEDIFF(year, c.birthdate, GETDATE()) AS age
-FROM gold.fact_sales s
-LEFT JOIN gold.dim_customers c
-	ON s.customer_key = c.customer_key
--- Filter for scope --
-WHERE order_date IS NOT NULL  -- Only consider valid order dates
+	SELECT
+		s.order_number,
+		s.product_key,
+		s.order_date,
+		s.sales_amount,
+		s.quantity,
+		c.customer_key,
+		c.customer_number,
+		CONCAT(c.first_name, ' ', c.last_name) AS customer_name, 
+		DATEDIFF(year, c.birthdate, GETDATE()) AS age
+	FROM gold.fact_sales s
+	LEFT JOIN gold.dim_customers c
+		ON s.customer_key = c.customer_key
+	-- Filter for scope --
+	WHERE order_date IS NOT NULL  -- Only consider valid order dates
 ),
 
+customer_aggregation AS (
 /*---------------------------------------------------------------------------
 2) Customer Aggregations: Summarizes key metrics at the customer level
 ---------------------------------------------------------------------------*/
-customer_aggregation AS (
 SELECT
 	customer_key,
 	customer_number,
@@ -75,40 +75,40 @@ GROUP BY
 )
 
 SELECT
-customer_key,
-customer_number,
-customer_name, 
-age,
--- Create age groups
-CASE
-	WHEN age < 20 THEN 'Under 20'
-	WHEN age BETWEEN 20 AND 29 THEN '20-29'
-	WHEN age BETWEEN 30 AND 39 THEN '30-39'
-	WHEN age BETWEEN 40 AND 49 THEN '40-49'
-	ELSE '50 and Above'
-END AS age_group,
--- Customer Segmentation
-CASE 
-    WHEN lifespan >= 12 AND total_sales > 5000 THEN 'VIP'
-    WHEN lifespan >= 12 AND total_sales <= 5000 THEN 'Regular'
-    ELSE 'New'
-END AS customer_segment,
-last_order_date,
--- Recency
-DATEDIFF(month, last_order_date, GETDATE()) AS recency_months,
-total_orders,
-total_sales,
-total_quantity,
-total_products,
-lifespan,
--- Average Order Value (AOV)
-CASE 
-	WHEN total_orders = 0 THEN 0
-	ELSE total_sales / total_orders 
-END AS avg_order_value,
--- Average Monthly Spend
-CASE
-	WHEN lifespan = 0 THEN total_sales
-	ELSE total_sales / lifespan
-END AS avg_monthly_spend
+	customer_key,
+	customer_number,
+	customer_name, 
+	age,
+	-- Create age groups
+	CASE
+		WHEN age < 20 THEN 'Under 20'
+		WHEN age BETWEEN 20 AND 29 THEN '20-29'
+		WHEN age BETWEEN 30 AND 39 THEN '30-39'
+		WHEN age BETWEEN 40 AND 49 THEN '40-49'
+		ELSE '50 and Above'
+	END AS age_group,
+	-- Customer Segmentation
+	CASE 
+		WHEN lifespan >= 12 AND total_sales > 5000 THEN 'VIP'
+		WHEN lifespan >= 12 AND total_sales <= 5000 THEN 'Regular'
+		ELSE 'New'
+	END AS customer_segment,
+	last_order_date,
+	-- Recency
+	DATEDIFF(month, last_order_date, GETDATE()) AS recency_months,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_products,
+	lifespan,
+	-- Average Order Value (AOV)
+	CASE 
+		WHEN total_orders = 0 THEN 0
+		ELSE total_sales / total_orders 
+	END AS avg_order_value,
+	-- Average Monthly Spend
+	CASE
+		WHEN lifespan = 0 THEN total_sales
+		ELSE total_sales / lifespan
+	END AS avg_monthly_spend
 FROM customer_aggregation;
